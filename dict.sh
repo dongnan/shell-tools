a41f7c60c4c1eac4c194bb0e2c5281bec13bcd14
dongnan <dongyh@126.com>
2016-05-27 13:29:08 +0800

    设置curl超时时间为5s

--- dict.sh
+++ dict.sh
@@ -9,4 +9,4 @@ else
     echo "[$word]"
 fi
 
-curl -s -e "http://3g.dict.cn/s.php" -A "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36" -H "Host:3g.dict.cn" "http://3g.dict.cn/s.php?q=$word" | perl -n -e '/<span>(.*)<\/span>(.*)<br\/>/ && print "$1 $2\n"'
+curl -m 5 -s -e "http://3g.dict.cn/s.php" -A "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36" -H "Host:3g.dict.cn" "http://3g.dict.cn/s.php?q=$word" | perl -n -e '/<span>(.*)<\/span>(.*)<br\/>/ && print "$1 $2\n"'
