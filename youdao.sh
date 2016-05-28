e0e0ac1f035019dba48dc9df150cc6deeea51d40
dongnan <dongyh@126.com>
2016-05-28 17:50:06 +0800

    调整输出

--- youdao.sh
+++ youdao.sh
@@ -6,7 +6,7 @@ then
     echo '请输入要查的单词:'
     read word
 else
-    echo "[$word]"
+    echo "   \"$word\""
 fi
 
-curl -m 5 -s -e "http://m.youdao.com/dict" -A "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36" -H "m.youdao.com" "http://m.youdao.com/dict?le=eng&q=$word" | perl -n -e '/<li>([^<]*)<\/li>/ && print "$1\n"'
+curl -m 5 -s -e "http://m.youdao.com/dict" -A "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36" -H "m.youdao.com" "http://m.youdao.com/dict?le=eng&q=$word" | perl -n -e '/<li>([^<]*)<\/li>/ && print "   $1\n"'
